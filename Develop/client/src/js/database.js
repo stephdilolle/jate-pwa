382a743569b8f9763ed5d02726319f66db93dacb
sdilolle <stephdilolle@gmail.com>
2024-07-31 21:51:49 -0400

    updated database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Putting content into the database');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // Add or update the content
+  await store.put({ id: 1, content });
+  await tx.done;
+  console.log('Content added to the database');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Getting content from the database');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  // Get all 
+  const allRecords = await store.getAll();
+  await tx.done;
+  console.log('Content retrieved from the database');
+  return allRecords;
+};
 
 initdb();
